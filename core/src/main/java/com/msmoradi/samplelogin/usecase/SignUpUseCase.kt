abe7e807f663491c1672fc2a7b41b952cb882095
FIRST_NAME LAST_NAME <mr.ms.moradi@gmail.com>
2020-12-24 10:36:09 +0330

    SignUpUseCase.kt added

---
+++ core/src/main/java/com/msmoradi/samplelogin/usecase/SignUpUseCase.kt
@@ -0,0 +1,18 @@
+package com.msmoradi.samplelogin.usecase
+
+import com.msmoradi.samplelogin.model.User
+import com.msmoradi.samplelogin.repository.UserRepository
+
+class SignUpUseCase(
+    private val userRepository: UserRepository
+) {
+    fun signUp(fullName: String, username: String, password: String) {
+        userRepository.insert(
+            User(
+                fullName = fullName,
+                username = username,
+                password = password
+            )
+        )
+    }
+}
