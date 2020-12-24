b049542c860a62cf795fb3568da033dd66565949
Saeed Moradi <mr.ms.moradi@gmail.com>
2020-12-24 23:06:24 +0330

    Dev refactor (#3)
    * add some extensions
    
    * Validator issue fixed
    
    * add email to user data class
    
    * prepare nav_graph.xml
    
    * editText replaced by TextInputLayout
    
    * add email to user class mapper
    
    * handle Login errors
    
    * fix validation and handle signUp errors
    
    * string res added
    
    * UserListFragment navigation issue fixed

--- app/src/main/java/com/msmoradi/samplelogin/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/msmoradi/samplelogin/ui/signup/SignUpFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.msmoradi.samplelogin.databinding.FragmentSignUpBinding
+import com.msmoradi.samplelogin.utils.disableErrorAfterTextChanged
+import com.msmoradi.samplelogin.utils.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,17 +33,39 @@ class SignUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.signUpButton.setOnClickListener {
-            signUpViewModel.signUp(
-                binding.fullNameTextField.editText?.text.toString(),
-                binding.userNameTextField.editText?.text.toString(),
-                binding.passwordTextField.editText?.text.toString(),
-                binding.confirmPasswordTextField.editText?.text.toString()
-            )
+        binding.apply {
+            signUpButton.setOnClickListener {
+                hideKeyboard()
+                onSignUpButtonClicked()
+            }
+            fullNameTextField.disableErrorAfterTextChanged()
+            userNameTextField.disableErrorAfterTextChanged()
+            emailTextField.disableErrorAfterTextChanged()
+            passwordTextField.disableErrorAfterTextChanged()
+            confirmPasswordTextField.disableErrorAfterTextChanged()
         }
 
-        signUpViewModel.loginNavigationObservable.observe(viewLifecycleOwner) {
-            navigateToLogin()
+        signUpViewModel.apply {
+            loginNavigationObservable.observe(viewLifecycleOwner) {
+                navigateToLogin()
+            }
+            fullNameErrorObservable.observe(viewLifecycleOwner, ::showFullNameError)
+            emailErrorObservable.observe(viewLifecycleOwner, ::showEmailError)
+            passwordErrorObservable.observe(viewLifecycleOwner, ::showPasswordNameError)
+            confirmPasswordErrorObservable.observe(viewLifecycleOwner, ::showConfirmPasswordError)
+            userNameErrorObservable.observe(viewLifecycleOwner, ::showUserNameError)
+        }
+    }
+
+    private fun onSignUpButtonClicked() {
+        binding.let {
+            signUpViewModel.signUp(
+                it.fullNameTextField.editText?.text.toString(),
+                it.userNameTextField.editText?.text.toString(),
+                it.emailTextField.editText?.text.toString(),
+                it.passwordTextField.editText?.text.toString(),
+                it.confirmPasswordTextField.editText?.text.toString()
+            )
         }
     }
 
@@ -48,5 +73,25 @@ class SignUpFragment : Fragment() {
         findNavController().navigate(
             SignUpFragmentDirections.actionSignUpFragmentToLoginFragment()
         )
+    }
+
+    private fun showFullNameError(@StringRes id: Int) {
+        binding.fullNameTextField.error = getString(id)
+    }
+
+    private fun showEmailError(@StringRes id: Int) {
+        binding.emailTextField.error = getString(id)
+    }
+
+    private fun showPasswordNameError(@StringRes id: Int) {
+        binding.passwordTextField.error = getString(id)
+    }
+
+    private fun showConfirmPasswordError(@StringRes id: Int) {
+        binding.confirmPasswordTextField.error = getString(id)
+    }
+
+    private fun showUserNameError(@StringRes id: Int) {
+        binding.userNameTextField.error = getString(id)
     }
 }
--- core/src/main/java/com/msmoradi/samplelogin/usecase/SignUpUseCase.kt
+++ core/src/main/java/com/msmoradi/samplelogin/usecase/SignUpUseCase.kt
@@ -6,11 +6,12 @@ import com.msmoradi.samplelogin.repository.UserRepository
 class SignUpUseCase(
     private val userRepository: UserRepository
 ) {
-    suspend fun signUp(fullName: String, username: String, password: String) {
+    suspend fun signUp(fullName: String, username: String, email: String, password: String) {
         userRepository.insert(
             User(
                 fullName = fullName,
                 username = username,
+                email = email,
                 password = password
             )
         )
