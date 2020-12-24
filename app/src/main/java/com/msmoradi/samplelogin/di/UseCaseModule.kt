a3dd791fc1c05b6e3ff757e6d80d470d4ed1bb21
FIRST_NAME LAST_NAME <mr.ms.moradi@gmail.com>
2020-12-24 10:39:04 +0330

    clear code and provide SignUpUseCase

--- app/src/main/java/com/msmoradi/samplelogin/di/UseCaseModule.kt
+++ app/src/main/java/com/msmoradi/samplelogin/di/UseCaseModule.kt
@@ -2,7 +2,9 @@ package com.msmoradi.samplelogin.di
 
 import com.msmoradi.samplelogin.data.datasourceimpl.LocalUserDataSourceImpl
 import com.msmoradi.samplelogin.repository.LoginRepository
+import com.msmoradi.samplelogin.repository.UserRepository
 import com.msmoradi.samplelogin.usecase.LoginUseCase
+import com.msmoradi.samplelogin.usecase.SignUpUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,16 +15,30 @@ import dagger.hilt.android.components.ApplicationComponent
 object UseCaseModule {
 
     @Provides
-    fun bindLoginRepository(
+    fun provideLoginRepository(
         localUserDataSource: LocalUserDataSourceImpl
     ): LoginRepository {
         return LoginRepository(localUserDataSource)
     }
 
     @Provides
-    fun bindLoginUseCase(
+    fun provideUserRepository(
+        localUserDataSource: LocalUserDataSourceImpl
+    ): UserRepository {
+        return UserRepository(localUserDataSource)
+    }
+
+    @Provides
+    fun provideLoginUseCase(
         loginRepository: LoginRepository
     ): LoginUseCase {
-        return LoginUseCase((loginRepository))
+        return LoginUseCase(loginRepository)
+    }
+
+    @Provides
+    fun provideSignUpUseCase(
+        userRepository: UserRepository
+    ): SignUpUseCase {
+        return SignUpUseCase(userRepository)
     }
 }
