f4db992310878278bf9a03a9e77d0180bbfcdc88
FIRST_NAME LAST_NAME <mr.ms.moradi@gmail.com>
2020-12-24 10:30:17 +0330

    Validator.kt added

---
+++ app/src/main/java/com/msmoradi/samplelogin/utils/Validator.kt
@@ -0,0 +1,13 @@
+package com.msmoradi.samplelogin.utils
+
+object Validator {
+
+
+    fun isPasswordValid(password: String): Boolean {
+        return true
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        return true
+    }
+}
